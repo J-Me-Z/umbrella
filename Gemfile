147f253fff246b558dc67f4083d913af319483ba
J-Me-Z <136928731+J-Me-Z@users.noreply.github.com>
2023-07-30 21:07:03 +0000

    more progress

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
 gem "http"
+gem "sinatra"
+gem "puma"
+gem "json"
--- umbrella.rb
+++ umbrella.rb
@@ -1,45 +1,62 @@
 
 pp "Where are you located?"
 
-user_location= gets.chomp.gsub(" ","%20")
+#user_location= gets.chomp.gsub(" ","%20")
+user_location = "Cleveland"
 
-pp user_location
+ pp user_location
 
-maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+ maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+ user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+ pp maps_url
 
 require "http"
 
 resp = HTTP.get(maps_url)
 
-raw_response = resp.to_s
-require "json"
-parsed_response =  JSON.parse(raw_response)
-results = parsed_response.fetch("results")
-first_result = pp results.at(0)
-pp geo= first_result.fetch("geometry")
+pp resp
 
-loc = geo.fetch("location")
+# raw_response = resp.to_s
+# require "json"
+# parsed_response =  JSON.parse(raw_response)
+# results = parsed_response.fetch("results")
+# first_result = pp results.at(0)
+# pp geo= first_result.fetch("geometry")
+
+# loc = geo.fetch("location")
+# first_result_hash = results_array.at(0)
+
+# geometry_hash = first_result_hash.fetch("geometry")
+
+# location_hash = geometry_hash.fetch("location")
+
+# latitude = location_hash.fetch("lat")
+
+# longitude = location_hash.fetch("lng")
+
+# puts "Your coordinates are #{latitude}, #{longitude}."
 
 
-# I've already created a string variable above: pirate_weather_api_key
-# It contains sensitive credentials that hackers would love to steal so it is hidden for security reasons.
 
-require "http"
+# # I've already created a string variable above: pirate_weather_api_key
+# # It contains sensitive credentials that hackers would love to steal so it is hidden for security reasons.
 
-# Assemble the full URL string by adding the first part, the API token, and the last part together
+# require "http"
 
-pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+# # Assemble the full URL string by adding the first part, the API token, and the last part together
 
-# Place a GET request to the URL
-raw_response = HTTP.get(pirate_weather_url)
+# pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+# pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
 
-require "json"
+# # Place a GET request to the URL
+# raw_response = HTTP.get(pirate_weather_url)
 
-parsed_response = JSON.parse(raw_response)
+# require "json"
 
-currently_hash = parsed_response.fetch("currently")
+# parsed_response = JSON.parse(raw_response)
 
-current_temp = currently_hash.fetch("temperature")
+# currently_hash = parsed_response.fetch("currently")
 
-puts "The current temperature is " + current_temp.to_s + "."
+# current_temp = currently_hash.fetch("temperature")
+
+# puts "The current temperature is " + current_temp.to_s + "."
